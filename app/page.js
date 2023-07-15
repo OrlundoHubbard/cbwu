cdc9d65de502238d098a20f46e27031d7a176e80
Orlundo Hubbard <weareprojectcode@gmail.com>
2023-07-15 17:29:39 -0500

    data-fetching

--- app/page.js
+++ app/page.js
@@ -1,8 +1,12 @@
 import Image from 'next/image'
+import main_image from "../public/main_image.png";
+import squiggle from "../public/squiggle.png";
 
 export default function Home() {
   return (
     <main>
+      <Image src={squiggle} width={50}/>
+      <Image src={main_image} width={800}/>
       <h1>Come Build With Us</h1>
     </main>
   )
